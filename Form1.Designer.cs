46c0c3a86c5f9a53921cfa5cf969d81a5428d303
Miracle_Kagari <xujianqiao@buaa.edu.cn>
2025-06-17 10:07:35 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WhoTmIsConflict
+{
+    partial class WhoTmConflict
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WhoTmConflict));
+            this.importVanillaTextButton = new System.Windows.Forms.Button();
+            this.importModTextButton = new System.Windows.Forms.Button();
+            this.compareTextButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // importVanillaTextButton
+            // 
+            this.importVanillaTextButton.Font = new System.Drawing.Font("宋体", 9F);
+            this.importVanillaTextButton.Location = new System.Drawing.Point(155, 229);
+            this.importVanillaTextButton.Name = "importVanillaTextButton";
+            this.importVanillaTextButton.Size = new System.Drawing.Size(212, 100);
+            this.importVanillaTextButton.TabIndex = 0;
+            this.importVanillaTextButton.Text = "导入原版文本";
+            this.importVanillaTextButton.UseVisualStyleBackColor = true;
+            this.importVanillaTextButton.Click += new System.EventHandler(this.importVanillaTextButton_Click);
+            // 
+            // importModTextButton
+            // 
+            this.importModTextButton.Font = new System.Drawing.Font("宋体", 9F);
+            this.importModTextButton.Location = new System.Drawing.Point(434, 229);
+            this.importModTextButton.Name = "importModTextButton";
+            this.importModTextButton.Size = new System.Drawing.Size(212, 100);
+            this.importModTextButton.TabIndex = 1;
+            this.importModTextButton.Text = "导入Mod文本";
+            this.importModTextButton.UseVisualStyleBackColor = true;
+            this.importModTextButton.Click += new System.EventHandler(this.importModTextButton_Click);
+            // 
+            // compareTextButton
+            // 
+            this.compareTextButton.Font = new System.Drawing.Font("宋体", 9F);
+            this.compareTextButton.Location = new System.Drawing.Point(703, 229);
+            this.compareTextButton.Name = "compareTextButton";
+            this.compareTextButton.Size = new System.Drawing.Size(212, 100);
+            this.compareTextButton.TabIndex = 2;
+            this.compareTextButton.Text = "比对";
+            this.compareTextButton.UseVisualStyleBackColor = true;
+            this.compareTextButton.Click += new System.EventHandler(this.compareTextButton_Click);
+            // 
+            // WhoTmConflict
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1093, 577);
+            this.Controls.Add(this.compareTextButton);
+            this.Controls.Add(this.importModTextButton);
+            this.Controls.Add(this.importVanillaTextButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "WhoTmConflict";
+            this.Text = "让我看看谁冲突了";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button importVanillaTextButton;
+        private System.Windows.Forms.Button importModTextButton;
+        private System.Windows.Forms.Button compareTextButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using YamlDotNet.Serialization;
+
+namespace WhoTmIsConflict
+{
+    public partial class WhoTmConflict : Form
+    {
+        private List<Tuple<string, string>> vanillaText;
+        private List<Tuple<string, string>> modText;
+
+
+        public WhoTmConflict()
+        {
+            InitializeComponent();
+        }
+
+        private void ImportDataFromFolder(string dialogTitle, ref List<Tuple<string, string>> importedData)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.Description = dialogTitle;
+
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string folderPath = folderBrowserDialog.SelectedPath;
+                    importedData = ImportDataFromYMLFiles(folderPath);
+                    MessageBox.Show($"成功导入{importedData.Count}个键值。", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private List<Tuple<string, string>> ImportDataFromYMLFiles(string folderPath)
+        {
+            List<Tuple<string, string>> data = new List<Tuple<string, string>>();
+
+            foreach (string filePath in Directory.GetFiles(folderPath, "*.yml"))
+            {
+                try
+                {
+                    using (StreamReader reader = new StreamReader(filePath))
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            int indexOfColon = line.IndexOf(":");
+                            if (indexOfColon != -1)
+                            {
+                                if(line.Substring(0, indexOfColon) != "l_english")
+                                {
+                                    string extractedValue = line.Substring(0, indexOfColon).Trim();
+                                    data.Add(new Tuple<string, string>(extractedValue, filePath));
+                                }
+                                
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"在处理文件{filePath}时发生错误：{ex.Message}", "错误提示", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            foreach (string subFolderPath in Directory.GetDirectories(folderPath))
+            {
+                data.AddRange(ImportDataFromYMLFiles(subFolderPath));
+            }
+
+            return data;
+        }
+
+        private List<Tuple<string, string, string>> CompareData(List<Tuple<string, string>> firstData, List<Tuple<string, string>> secondData)
+        {
+            List<Tuple<string, string, string>> conflicts = new List<Tuple<string, string, string>>();
+
+            foreach (var firstTuple in firstData)
+            {
+                foreach (var secondTuple in secondData)
+                {
+                    if (firstTuple.Item1 == secondTuple.Item1)
+                    {
+                        conflicts.Add(new Tuple<string, string, string>(
+                            firstTuple.Item1,
+                            firstTuple.Item2,
+                            secondTuple.Item2
+                        ));
+                    }
+                }
+            }
+
+            return conflicts;
+        }
+
+        private void importVanillaTextButton_Click(object sender, EventArgs e)
+        {
+            ImportDataFromFolder("请选择原版文本所在文件夹，选择english或simp_chinese文件夹", ref vanillaText);
+
+        }
+
+        private void importModTextButton_Click(object sender, EventArgs e)
+        {
+            ImportDataFromFolder("请选择Mod文本所在文件夹，选择english或simp_chinese文件夹", ref modText);
+
+        }
+
+        private void compareTextButton_Click(object sender, EventArgs e)
+        {
+            if (vanillaText == null || modText == null)
+            {
+                MessageBox.Show("请先导入原版文本与Mod文本！", "缺少文本", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            List<Tuple<string, string, string>> conflicts = CompareData(vanillaText, modText);
+
+            if (conflicts.Count > 0)
+            {
+                // 冲突存在，输出到文件
+                using (StreamWriter writer = new StreamWriter("冲突键值.txt"))
+                {
+                    foreach (var conflict in conflicts)
+                    {
+                        writer.WriteLine($"{conflict.Item1}\t{Path.GetFileName(conflict.Item2)}\t{Path.GetFileName(conflict.Item3)}");
+                    }
+                }
+
+                MessageBox.Show("存在冲突，键值与文件已输出到冲突键值.txt文件中。", "存在冲突", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("无键值冲突！", "没冲突", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
